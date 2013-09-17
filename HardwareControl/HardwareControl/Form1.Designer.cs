7e0a02826ce02be3d1592d87eb3f1f9d865990db
Lipotam <Lipotam@gmail.com>
2013-09-17 12:18:52 +0300

    Lab 1 and 2 frontend set up

---
+++ HardwareControl/HardwareControl/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace HardwareControl
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.shemaPictureBox = new System.Windows.Forms.PictureBox();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.testsListView = new System.Windows.Forms.ListView();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.button2 = new System.Windows.Forms.Button();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			((System.ComponentModel.ISupportInitialize)(this.shemaPictureBox)).BeginInit();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// shemaPictureBox
+			// 
+			this.shemaPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.shemaPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("shemaPictureBox.Image")));
+			this.shemaPictureBox.Location = new System.Drawing.Point(6, 6);
+			this.shemaPictureBox.Name = "shemaPictureBox";
+			this.shemaPictureBox.Size = new System.Drawing.Size(785, 416);
+			this.shemaPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.shemaPictureBox.TabIndex = 0;
+			this.shemaPictureBox.TabStop = false;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(3, 2);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(1166, 483);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.shemaPictureBox);
+			this.tabPage1.Controls.Add(this.groupBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(1158, 457);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Lab1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(330, 428);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(166, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "OneDimention Generate Tests";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.testsListView);
+			this.groupBox1.Location = new System.Drawing.Point(809, 6);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(342, 416);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Sets";
+			// 
+			// testsListView
+			// 
+			this.testsListView.FullRowSelect = true;
+			this.testsListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+			this.testsListView.Location = new System.Drawing.Point(9, 19);
+			this.testsListView.Name = "testsListView";
+			this.testsListView.Size = new System.Drawing.Size(327, 391);
+			this.testsListView.TabIndex = 1;
+			this.testsListView.UseCompatibleStateImageBehavior = false;
+			this.testsListView.View = System.Windows.Forms.View.Details;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.pictureBox1);
+			this.tabPage2.Controls.Add(this.button2);
+			this.tabPage2.Controls.Add(this.groupBox2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(1158, 457);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Lab2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(7, 6);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(785, 416);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// listView1
+			// 
+			this.listView1.FullRowSelect = true;
+			this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+			this.listView1.Location = new System.Drawing.Point(9, 19);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(327, 391);
+			this.listView1.TabIndex = 1;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(331, 428);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(166, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "MultiDimention Generate Tests";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.listView1);
+			this.groupBox2.Location = new System.Drawing.Point(810, 6);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(342, 416);
+			this.groupBox2.TabIndex = 5;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Sets";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1170, 497);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.shemaPictureBox)).EndInit();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.groupBox1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.groupBox2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox shemaPictureBox;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListView testsListView;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.ListView listView1;
+	}
+}
+
