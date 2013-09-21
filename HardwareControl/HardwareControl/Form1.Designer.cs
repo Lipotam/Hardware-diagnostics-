a7acb7bd17f01a7ef63a7a1750a4b9d93e897f59
Lipotam <Lipotam@gmail.com>
2013-09-21 09:20:58 +0300

    Lab3 interface added

--- HardwareControl/HardwareControl/Form1.Designer.cs
+++ HardwareControl/HardwareControl/Form1.Designer.cs
@@ -30,7 +30,7 @@
 		{
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.shemaPictureBox = new System.Windows.Forms.PictureBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Lab3Tab = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -40,13 +40,26 @@
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.defectsListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.minDefectsListView = new System.Windows.Forms.ListView();
+            this.startButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.shemaPictureBox)).BeginInit();
-            this.tabControl1.SuspendLayout();
+            this.Lab3Tab.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // shemaPictureBox
@@ -60,15 +73,16 @@
             this.shemaPictureBox.TabIndex = 0;
             this.shemaPictureBox.TabStop = false;
             // 
-            // tabControl1
+            // Lab3Tab
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(3, 2);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1166, 483);
-            this.tabControl1.TabIndex = 0;
+            this.Lab3Tab.Controls.Add(this.tabPage1);
+            this.Lab3Tab.Controls.Add(this.tabPage2);
+            this.Lab3Tab.Controls.Add(this.tabPage3);
+            this.Lab3Tab.Location = new System.Drawing.Point(3, 2);
+            this.Lab3Tab.Name = "Lab3Tab";
+            this.Lab3Tab.SelectedIndex = 0;
+            this.Lab3Tab.Size = new System.Drawing.Size(1166, 497);
+            this.Lab3Tab.TabIndex = 0;
             // 
             // tabPage1
             // 
@@ -169,22 +183,114 @@
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.startButton);
+            this.tabPage3.Controls.Add(this.minDefectsListView);
+            this.tabPage3.Controls.Add(this.groupBox3);
+            this.tabPage3.Controls.Add(this.pictureBox2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1158, 471);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Lab3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(5, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(316, 181);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.defectsListView);
+            this.groupBox3.Location = new System.Drawing.Point(327, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(824, 187);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Defects for All Sets";
+            // 
+            // defectsListView
+            // 
+            this.defectsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.defectsListView.FullRowSelect = true;
+            this.defectsListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.defectsListView.Location = new System.Drawing.Point(7, 14);
+            this.defectsListView.Name = "defectsListView";
+            this.defectsListView.Size = new System.Drawing.Size(811, 167);
+            this.defectsListView.TabIndex = 0;
+            this.defectsListView.UseCompatibleStateImageBehavior = false;
+            this.defectsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Modeling Set";
+            this.columnHeader1.Width = 220;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Const 0";
+            this.columnHeader2.Width = 143;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Const 1";
+            this.columnHeader3.Width = 134;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "N";
+            this.columnHeader4.Width = 20;
+            // 
+            // minDefectsListView
+            // 
+            this.minDefectsListView.FullRowSelect = true;
+            this.minDefectsListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.minDefectsListView.Location = new System.Drawing.Point(5, 196);
+            this.minDefectsListView.Name = "minDefectsListView";
+            this.minDefectsListView.Size = new System.Drawing.Size(1140, 243);
+            this.minDefectsListView.TabIndex = 3;
+            this.minDefectsListView.UseCompatibleStateImageBehavior = false;
+            this.minDefectsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(5, 445);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(71, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1170, 497);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.Lab3Tab);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.shemaPictureBox)).EndInit();
-            this.tabControl1.ResumeLayout(false);
+            this.Lab3Tab.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
 		}
@@ -192,7 +298,7 @@
 		#endregion
 
 		private System.Windows.Forms.PictureBox shemaPictureBox;
-		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabControl Lab3Tab;
 		private System.Windows.Forms.TabPage tabPage1;
 		private System.Windows.Forms.TabPage tabPage2;
 		private System.Windows.Forms.Button button1;
@@ -202,6 +308,16 @@
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.GroupBox groupBox2;
 		private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ListView minDefectsListView;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView defectsListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button startButton;
 	}
 }
 
